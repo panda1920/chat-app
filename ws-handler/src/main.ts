429115a06c8b13c84299db912c8256530836ba51
Kei Kamigama <15102478+panda1920@users.noreply.github.com>
2025-01-25 03:19:58 +0900

    Added custom http server for ws-handler

--- ws-handler/src/main.ts
+++ ws-handler/src/main.ts
@@ -1,20 +1,46 @@
+import { createServer } from 'node:http'
 import { WebSocketServer } from 'ws'
 
-const wss = new WebSocketServer({
-  port: 3000,
-})
-
-wss.on('connection', (ws) => {
-  ws.on('error', console.error)
-
-  console.log('You have made a connection!')
-
-  ws.on('message', (data) => {
-    console.log(`You have received a following data!: ${data}`)
-    ws.send('Thank you for the data :)')
+function main() {
+  const server = createServer()
+  const wss = new WebSocketServer({
+    noServer: true,
+    // port: 3000,
   })
-})
 
-wss.on('listening', () => {
-  console.log('listening...')
-})
+  // websocket setting
+  wss.on('connection', (ws, req) => {
+    ws.on('error', console.error)
+
+    console.log('You have made a connection!')
+
+    ws.on('message', (data) => {
+      console.log(`You have received a following data!: ${data}`)
+      ws.send('Thank you for the data :)')
+    })
+  })
+
+  // http server setting
+  server.on('upgrade', (req, socket, head) => {
+    socket.on('error', console.error)
+
+    // put some sort of authentication here
+    console.log(`headers: ${JSON.stringify(req.headers)}`)
+    console.log(`method: ${JSON.stringify(req.method)}`)
+    console.log(`url: ${JSON.stringify(req.url)}`)
+
+    wss.handleUpgrade(req, socket, head, (ws) => {
+      wss.emit('connection', ws, req)
+    })
+  })
+  server.on('error', (err) => {
+    console.error(err)
+  })
+
+  const port = 3000
+  server.listen(port, '0.0.0.0', () =>
+    console.log(`Listening on port: ${port}`),
+  )
+}
+
+main()
