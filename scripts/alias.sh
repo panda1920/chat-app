5516796b1e6257b1b5a5a25288f5bbd62061f913
Kei Kamigama <15102478+panda1920@users.noreply.github.com>
2025-02-13 05:13:54 +0900

    fixed startup script

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -12,18 +12,76 @@ alias logs="$COMPOSE_COMMAND logs"
 alias sso-login="${BACKEND_EXEC_BASH} 'aws sso login --use-device-code'"
 alias reload=". $CUR_DIR/alias.sh"
 
+# start with local dynamo
 function up() {
-  local envFile="$ROOT_DIR/env/.env.local";
+  upApp "$ROOT_DIR/scripts/awsenv/.env.local"
+}
+
+# start with dynamo in dev 
+function updev() {
+  upApp "$ROOT_DIR/scripts/awsenv/.env.dev"
+}
+
+# first param - awsenv file
+# insert awsenv information to env file and starts the app
+function upApp() {
+  local awsFile="$1"
+  local backendEnv="$ROOT_DIR/backend/.env"
+  local wsHandlerEnv="$ROOT_DIR/ws-handler/.env"
+
+  # check file existance
+  if [ ! -f "$awsFile" ]; then
+    echo -e "File $awsFile not found\nCreate the file and try again"
+    return 1
+  fi
+  if [ ! -f "$backendEnv" ]; then
+    echo -e "File $backendEnv not found\nCreating from example file..."
+    cp $ROOT_DIR/backend/.env.example $backendEnv
+  fi
+  if [ ! -f "$wsHandlerEnv" ]; then
+    echo -e "File $wsHandlerEnv not found\nCreating from example file..."
+    cp $ROOT_DIR/ws-handler/.env.example $wsHandlerEnv
+  fi
+
+  replaceAWSSection $backendEnv $awsFile
+  replaceAWSSection $wsHandlerEnv $awsFile
   
-  cp $envFile "$ROOT_DIR/backend/.env";
   $COMPOSE_COMMAND up -d;
 }
 
-function upaws() {
-  local envFile="$ROOT_DIR/env/.env.local.aws";
-  
-  cp $envFile "$ROOT_DIR/backend/.env";
-  $COMPOSE_COMMAND up -d;
+# first param - file to edit
+# second param - content of file to replace with
+function replaceAWSSection () {
+  # check file existance
+  if [ ! -f "$1" ]; then
+    echo -e "file $1 does not exist.\nCreate the file and try again"
+    return 1
+  fi
+  if [ ! -f "$2" ]; then
+    echo -e "file $2 does not exist.\nCreate the file and try again"
+    return 1
+  fi
+
+  # find first and last occurent of the keyword AWS
+  local firstLine=$(grep -n "AWS" "$1" | head -n1 | cut -d: -f1)
+  local lastLine=$(grep -n "AWS" "$1" | tail -n1 | cut -d: -f1)
+
+  # delete only when match is found
+  if [ -n "$firstLine" ]; then
+    sed -i "${firstLine},${lastLine}d" "$1"
+  fi
+
+  # insert content of replacement file
+  if [ -z "$firstLine" ] || [ "$firstLine" -gt "$(wc -l < $1)" ]; then
+    # just append when file is too short to be inserted
+    cat "$2" >> "$1"
+  else
+    # insert into the specific line number
+    # use placeholder to prevent uninteded overwrites
+    local placeholder="### placeholder"
+    sed -i "${firstLine}i ${placeholder}" "$1"
+    sed -i -e "/${placeholder}/r $2" -e "/${placeholder}/d" $1
+  fi
 }
 
 function goin() {
